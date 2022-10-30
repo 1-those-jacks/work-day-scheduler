46f123e95efa3f4ef791f699c37997931df04586
1-those-jacks <85271008+1-those-jacks@users.noreply.github.com>
2022-10-30 18:55:05 -0500

    README Updated

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# work-day-scheduler
-Repository for the Week #5 homework challenge due 10/31/2022
+# Assignment: 05 Challenge Work Day Scheduler
+
+## Description
+
+The application is a daily planner. The application will take in inputs at each time and save to local storage when the save button on the right of each corresponding hour is pressed. The planner tracks the current date and is automatically populated at the top of the page. The hours of the day are also tracked. Depending on the time that the user uses the application, set styling will highlight whether the hour has passed, is the current hour, or the hour is in the future. 
+
+## Screenshot
+
+## Link
+GitHub Repository: https://github.com/1-those-jacks/work-day-scheduler 
+GitHub Page: https://1-those-jacks.github.io/work-day-scheduler/
+
--- script.js
+++ script.js
@@ -3,35 +3,36 @@ $("#current-day").text(moment().format('dddd, MMMM Do YYYY'));
 // Functions for picking the time and selecting it if it is past, present, or future
 var presentTime = moment().hours();
 var timeBlockElems = document.querySelectorAll(".time-block");
-console.log(presentTime);
-console.log(timeBlockElems);
+// console.log(presentTime);
+// console.log(timeBlockElems);
 // Functions for saving data logged in the textarea and putting into local storage...
 function saveToTimeSlot(){
 	console.log(JSON.parse(localStorage.getItem("time")));
 	console.log(timeBlockElems[0].children);
 	// need to set item and loop through to get the value attribute from the text area with local storage....
-	if (timeBlockElems[0].children[1].value) {
-		console.log(timeBlockElems[0].children[1].value);
-		localStorage.setItem(".time-block", timeBlockElems[0].children[1].value);
+	for (var i = 0; i < timeBlockElems.length; i++) {
+	if (timeBlockElems[i].children[1].value) {
+		console.log(timeBlockElems[i].children[1].value);
+		localStorage.setItem("time[i]", timeBlockElems[i].children[1].value);
 	}
+}
 }
 
 function loadToTimeSlot(){
-	console.log(JSON.parse(localStorage.getItem("time")));
+	console.log(localStorage.getItem("time"));
 	var hasData = JSON.parse(localStorage.getItem("time"));
 	if (hasData) {
 		timeBlockElems.forEach((elem, idx) => {
-			console.log("Line 18 ", elem, idx);
+			console.log("Line 24 ", elem, idx);
 			elem.textContent = localStorage.getItem(elem.getAttribute("id"));
-		})
+		});
+		}
 	}
-}
 
 function updateTime() {
-	var currHr = moment().format("HH:mm");
-	console.log(typeof presentTime);
-	for (var i = 0; i<timeBlockElems.length; i++) {
-		console.log(timeBlockElems[i]);
+	// console.log(typeof presentTime);
+	for (var i = 0; i < timeBlockElems.length; i++) {
+		// console.log(timeBlockElems[i]);
 		var currBlock = timeBlockElems[i];
 		var currBlockTime = parseInt(currBlock.getAttribute("id"));
 		if (currBlockTime < presentTime) {
@@ -48,4 +49,4 @@ function updateTime() {
 
 // setInterval(updateTime, 15000);
 updateTime();
-document.addEventListener("load", loadToTimeSlot);
+loadToTimeSlot();
